0b14e1d7484d2036cf5421bcd78c867441880404
Nick Latcham <nick.latcham@proton.me>
2023-05-07 17:38:41 -0500

    .

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,6 @@
-import { useState } from "react";
 import Swap from "./Swap";
 
 const Header = () => {
-  const [menu, setMenu] = useState(false);
-
   return (
     <header className="flex items-center justify-between text-center w-full mt-2.5 mb-5 sm:mb-10">
       <p className="text-2xl font-bold text-center drop-shadow-lg sm:text-4xl bg-gradient-to-r from-indigo-500 via-fuchsia-500 to-rose-400 text-transparent bg-clip-text">

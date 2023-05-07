c8368fa40724ec3b669f3f633d883888b4a0e651
Nick Latcham <nick.latcham@proton.me>
2023-05-07 17:31:33 -0500

    refactor (ui): update colors

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <div className='min-w-full min-h-screen mx-auto px-5 bg-gradient-to-b from-dark_blue to-purple bg-cover'>
+    <div className="min-w-full min-h-screen mx-auto px-5 bg-gradient-to-b from-black via-dark_blue/50 to-purple bg-cover">
       <App />
     </div>
   </React.StrictMode>

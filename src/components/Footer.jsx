a71e98e037b73051ac45a0ff344816b9bdd4c88b
Nick Latcham <nick.latcham@proton.me>
2023-05-07 17:44:00 -0500

    .

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,8 @@
 const Footer = () => {
   return (
-    <footer className='container flex justify-center mt-10'>
-      <p className='text-sm text-gray-400 sm:ml-4 sm:pl-4  sm:py-2 sm:mt-0 mt-4'>
-        © 2023 Nick Latcham — @Nick_latcham
+    <footer className="flex justify-center mt-10">
+      <p className="text-sm text-gray-400 sm:ml-4 sm:pl-4  sm:py-2 sm:mt-0 mt-4">
+        © 2023 Nick Latcham — <a href="https://github.com/nicklatch">Github</a>
       </p>
     </footer>
   );

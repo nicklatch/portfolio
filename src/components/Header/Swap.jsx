bdf020b9a0a36feaa352c089a59da37bd1e0dc7f
Nick Latcham <nick.latcham@proton.me>
2023-06-24 15:14:27 -0500

    feat(routing): add react-router

--- src/components/Header/Swap.jsx
+++ src/components/Header/Swap.jsx
@@ -1,4 +1,5 @@
 import { useRef } from 'react';
+import { Link } from 'react-router-dom';
 
 const Swap = () => {
   const dropdown = useRef();
@@ -8,30 +9,30 @@ const Swap = () => {
   };
 
   return (
-    <div className='dropdown dropdown-bottom' ref={dropdown}>
+    <div className="dropdown dropdown-bottom" ref={dropdown}>
       <button
-        tabIndex='0'
-        className='btn btn-ghost bg-transparent ml-1 hover:bg-transparent'
+        tabIndex="0"
+        className="btn btn-ghost bg-transparent ml-1 hover:bg-transparent"
         onClick={handleClick}
       >
         Menu
       </button>
       <ul
-        tabIndex='0'
-        className='menu dropdown-content rounded-md w-fit items-center justify-center bg-grey-800 dark:bg-black ring-indigo-500 ring-2 shadow-xl shadow-indigo-500'
+        tabIndex="0"
+        className="menu dropdown-content rounded-md w-fit items-center justify-center bg-grey-800 dark:bg-black ring-indigo-500 ring-2 shadow-xl shadow-indigo-500"
       >
-        <li className='w-full sm:hover-bordered'>
-          <a
-            href='#about'
-            className='text-center w-full text-white pl-5 m-auto'
+        <li className="w-full sm:hover-bordered">
+          <Link
+            to="/about"
+            className="text-center w-full text-white pl-5 m-auto"
           >
             About
-          </a>
+          </Link>
         </li>
-        <li className='w-full sm:hover-bordered'>
-          <a href='#projects' className='text-center m-auto text-white'>
+        <li className="w-full sm:hover-bordered">
+          <Link to="/projects" className="text-center m-auto text-white">
             Projects
-          </a>
+          </Link>
         </li>
       </ul>
     </div>

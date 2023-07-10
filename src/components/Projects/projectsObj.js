b4fc290e2f69f2924716fb97dbbda3b0620ea928
Nick <nick.latcham@proton.me>
2023-07-10 14:36:27 -0500

    updated projects

--- src/components/Projects/projectsObj.js
+++ src/components/Projects/projectsObj.js
@@ -1,4 +1,5 @@
 import socialDash from '../../assets/images/fem_socMedDash.png';
+import timeDash from '../../assets/images/fem_timeTrack.png';
 
 export const projects = [
   {
@@ -9,5 +10,13 @@ export const projects = [
       'https://front-end-mentor-social-media-dash-20pi71e0h-nicklatch.vercel.app/?vercelToolbarCode=uWsYHtlvg6CusaJ',
     repo:
       'https://github.com/nicklatch/frontEndMentor/tree/main/social-media-dash',
+  },
+  {
+    title: 'Front End Mentor Challalange - Time Tracking Dash',
+    screenshot: timeDash,
+    desc: 'My solution to the Time Tracking Dash Challange',
+    live: 'https://fementor-time-tracking-dashboard.vercel.app/',
+    repo:
+      'https://github.com/nicklatch/frontEndMentor/tree/main/time-tracking-dash',
   },
 ];

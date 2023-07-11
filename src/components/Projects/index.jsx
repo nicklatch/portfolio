e092741d2a77e3c4e0210ae720c10cf19f38d6b5
Nick <nick.latcham@proton.me>
2023-07-11 09:39:16 -0500

    fix(key): add key to iterated component
    resolves warning message

--- src/components/Projects/index.jsx
+++ src/components/Projects/index.jsx
@@ -7,7 +7,7 @@ const Projects = () => {
       <h1 className='page__heading'>Projects</h1>
       <main className='flex flex-col justify-center items-center text-center'>
         {projects.map((project) => (
-          <ProjectCard {...project} />
+          <ProjectCard key={project.repo} {...project} />
         ))}
       </main>
     </>

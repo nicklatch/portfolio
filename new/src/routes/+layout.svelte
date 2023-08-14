910b1aa1aee07ae4a5f3b78e742b6d7eb2e8e812
Nick Latcham <nick.latcham@proton.me>
2023-08-13 20:47:31 -0500

    initial

---
+++ new/src/routes/+layout.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import type { LayoutData } from './$types';
+</script>
+
+<header>
+	<nav>
+		<ul>
+			<li>
+				<a href="/about">About</a>
+			</li>
+		</ul>
+	</nav>
+</header>
+<main><slot /></main>
+<footer>footer</footer>

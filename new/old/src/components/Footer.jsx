11f9b4f98715949ea400a21e87a804100c780968
Nick Latcham <nick.latcham@proton.me>
2023-08-16 21:12:17 -0500

    .

--- new/old/src/components/Footer.jsx
+++
@@ -1,8 +0,0 @@
-const Footer = () => {
-  return (
-    <footer id='footer'>
-      © 2023 Nick Latcham — <a href='https://github.com/nicklatch'>Github</a>
-    </footer>
-  );
-};
-export default Footer;

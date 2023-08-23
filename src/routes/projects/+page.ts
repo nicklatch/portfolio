c9d27cbc04e46037d27ff6fffda1fc3858438617
Nick <96550850+nicklatch@users.noreply.github.com>
2023-08-22 21:30:30 -0500

    Update +page.ts
    Spelling

--- src/routes/projects/+page.ts
+++ src/routes/projects/+page.ts
@@ -5,7 +5,7 @@ export const load: PageLoad = ({ params }) => {
   const projects: Array<Project> = [
     {
       title: 'FastLane Fusion',
-      description: "FastLane Fusion is an all-in-one race management SaaS. It makes the process of registration, heat/feature lineup creation, results entry, points allocation, and payouts a breeze! This project is still very you young and in the early stages.",
+      description: "FastLane Fusion is an all-in-one race management SaaS. It makes the process of registration, heat/feature lineup creation, results entry, points allocation, and payouts a breeze! This project is still very young and in the early stages.",
       screenshot: { src: flfScreenshot, alt: "A screenshot of a FastLaneFusion instance" }
     },
   ];
